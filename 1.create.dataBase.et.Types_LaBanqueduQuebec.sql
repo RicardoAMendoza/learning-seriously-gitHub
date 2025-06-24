6c7fbf2e732413cd41554bb06177c008519b843a
Ricardo Mendoza <omnimtv@gmail.com>
2025-06-23 22:01:17 -0400

    Update 1.create.dataBase.et.Types_LaBanqueduQuebec.sql
    Despues e estas pruebas vamos a agregar todo el proyecto de sqlServer para darle fin

--- 1.create.dataBase.et.Types_LaBanqueduQuebec.sql
+++ 1.create.dataBase.et.Types_LaBanqueduQuebec.sql
@@ -1,7 +1,7 @@
 -- This project uses the following licenses:
 -- MIT License
 -- Copyright (c) 2024 Ricardo Alfredo Mendoza Cortes 
--- Montréal Québec Canada
+-- MontrÃ©al QuÃ©bec Canada
 
 -- Repository : MVC_winapp_NationalBank_SqlServer > https://github.com/RicardoAMendoza/MVC_winapp_NationalBank_SqlServer.git
 -- Project : prjWeb_MVC_LaBanquenationale_Rm
@@ -26,3 +26,5 @@ create type tv100 from VARCHAR(100) NULL
 create type tmoney from money NULL
 create type tv3 from VARCHAR(3) NULL
 create type tv1 from VARCHAR(1) NULL
+
+-- Este es un proyecto solo para aprender la maestria de GitHub
